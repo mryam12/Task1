db00dcddf07fb406666f6f9268e27ae312e281f5
mryam12 <pink_rose9896@hotmail.com>
2022-08-05 09:38:02 +0200

    The result now will be equal 17.

--- File.kt
+++ File.kt
@@ -1,7 +1,8 @@
 fun (){
-    var number1 = 2
+    var number1 = 7
     var number2 = 10
     var result = number1 + number2
     //The result will now change since number2 has changed.
+    //The result will change again because number1 is changed.
     println(result)
 }
